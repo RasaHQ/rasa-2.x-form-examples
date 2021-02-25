9aca8d6036e84cd6028554688d44280ee3f21fd2
koaning <vincentwarmerdam@gmail.com>
2021-02-25 16:39:39 +0100

    custom-form-works-hurrah

---
+++ 07-custom-name-experience/actions/actions.py
@@ -0,0 +1,80 @@
+import yaml 
+import pathlib 
+from typing import Text, List, Any, Dict, Optional
+
+from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+
+names = pathlib.Path("data/names.txt").read_text().split("\n")
+
+
+class ValidateNameForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_name_form"
+    
+    async def required_slots(
+        self,
+        slots_mapped_in_domain: List[Text],
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: "DomainDict",
+    ) -> Optional[List[Text]]:
+        first_name = tracker.slots.get("first_name")
+        if first_name is not None:
+            if tracker.slots.get("first_name") not in names:
+                return ["name_spelled_correctly"] + slots_mapped_in_domain
+        return slots_mapped_in_domain
+    
+    async def extract_name_spelled_correctly(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> Dict[Text, Any]:
+        intent = tracker.get_intent_of_latest_message()
+        return {"name_spelled_correctly": intent == "affirm"}
+
+    def validate_name_spelled_correctly(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `first_name` value."""
+        if tracker.get_slot("name_spelled_correctly"):
+            return {"first_name": tracker.get_slot("first_name"), "name_spelled_correctly": True}
+        dispatcher.utter_message(text="Let's try that again then.")
+        return {"first_name": None, "name_spelled_correctly": None}
+        
+    def validate_first_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `first_name` value."""
+
+        # If the name is super short, it might be wrong.
+        print(f"First name given = {slot_value} length = {len(slot_value)}")
+        if len(slot_value) <= 2:
+            dispatcher.utter_message(text=f"That's a very short name. I'm assuming you mis-spelled.")
+            return {"first_name": None}
+        else:
+            return {"first_name": slot_value}
+
+    def validate_last_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `last_name` value."""
+
+        # If the name is super short, it might be wrong.
+        print(f"Last name given = {slot_value} length = {len(slot_value)}")
+        if len(slot_value) <= 2:
+            dispatcher.utter_message(text=f"That's a very short name. I'm assuming you mis-spelled.")
+            return {"last_name": None}
+        else:
+            return {"last_name": slot_value}
